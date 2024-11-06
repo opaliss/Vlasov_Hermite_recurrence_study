9125b5db14716fbd69bb3dda0f88b40e90c5b3e9
opalissan <opal.issan@gmail.com>
2024-11-06 09:07:06 -0800

    update filter response function to be efficient

--- aw_hermite/R_AW_filter.py
+++ aw_hermite/R_AW_filter.py
@@ -3,7 +3,7 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
 
 import sympy
-from sympy import banded, symbols, SparseMatrix
+from sympy import symbols
 import numpy as np
 import scipy
 import pickle
@@ -27,24 +27,23 @@ for Nv in np.arange(4, 14, 2):
     vec2 = sympy.zeros(Nv)
     for nn in range(0, Nv + 1):
         # hyper collisions coefficient
-        vec2[nn] = sympy.Pow(nn/(Nv-1), 36)
+        vec2[nn] = np.power(nn/(Nv-1), 36) / np.sqrt(2)
         if vec2[nn] < 1e-16:
             vec2[nn] = 0
 
-    # enforce k=1 for simplicity now
-    k = 1
-
     # create an advection tri-diagonal matrix
-    A = banded({1: tuple(vec[0, :-1]), -1: tuple(vec[0, :-1]), 0: tuple(nu * vec2[0, :] / (sympy.I * sympy.sqrt(2) * k))})
+    A = sympy.banded({1: tuple(vec[0, :-1]),
+                     -1: tuple(vec[0, :-1]),
+                      0: tuple(nu * vec2[0, :] / sympy.I)})
 
     # identity matrix
-    I = sympy.eye(Nv, dtype=int)
+    I = sympy.eye(Nv)
 
     # invert matrix
-    M = sympy.SparseMatrix(I * xi - k / np.abs(k) * A)
+    M = sympy.simplify(sympy.SparseMatrix(I * xi - A))
 
     # get final response function
-    R_approx = sympy.simplify(sympy.simplify(M.inv()[0, 1] / sympy.sqrt(2) * k / np.abs(k)))
+    R_approx = sympy.simplify(M.inv()[0, 1] / sympy.sqrt(2))
     print("I successfully inverted the matrix! ")
 
     asymptotics_0 = R_approx.series(xi, 0, 2, dir="+")
